7c95d731246452df8b880ec1d3aecdbed09dc561
changeonPark <qkrcksrjs@gmail.com>
2023-03-12 22:46:43 +0900

    feat: JS map higher-order function으로 구현

---
+++ src/ch03/higher-order-function.ts
@@ -0,0 +1,9 @@
+export const map = <A, B>(array: Array<A>, f: (a: A) => B): Array<B> => {
+  const result: Array<B> = [];
+
+  for (const value of array) {
+    result.push(f(value));
+  }
+
+  return result;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
 // import { main } from "./ch02";
 import { calcList } from "./ch03";
 
+import { map } from "./ch03/higher-order-function";
+
+const array = [
+  {
+    id: 1,
+    name: "홍길동",
+  },
+  {
+    id: 2,
+    name: "김길동",
+  },
+  {
+    id: 3,
+    name: "이길동",
+  },
+];
+
+console.log(map<{ id: number; name: string }, string>(array, item => item.name));
+
 calcList();
