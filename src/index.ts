58e5e581e3f61d47f93268ad8f0e22b5359f6789
changeonPark <qkrcksrjs@gmail.com>
2023-03-28 17:39:35 +0900

    feat: 2중 for문 map으로 전환

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,10 @@
 // import { main } from "./ch02";
-// import { map } from "basic/ch03/higher-order-function";
-import { myCart } from "basic/ch03";
-import { cart } from "basic/ch03/cart";
 
-// const array = [
-//   {
-//     id: 1,
-//     name: "홍길동",
-//   },
-//   {
-//     id: 2,
-//     name: "김길동",
-//   },
-//   {
-//     id: 3,
-//     name: "이길동",
-//   },
-// ];
+// import { myCart } from "basic/ch03";
+// import { cart } from "basic/ch03/cart";
 
-// console.time("performance");
-// console.log(
-//   map<{ id: number; name: string }, string>(array, (item, index) => {
-//     console.log("index: ", index);
-//     return item.name;
-//   })
-// );
-// console.timeEnd("performance");
+// const { cartInfo, totalPriceInfo, totalCountInfo } = myCart(cart);
+// console.log(cartInfo, "\n", totalPriceInfo, "\n", totalCountInfo);
 
-const { cartInfo, totalPriceInfo, totalCountInfo } = myCart(cart);
-console.log(cartInfo, "\n", totalPriceInfo, "\n", totalCountInfo);
+import init from "basic/ch03/card";
+init();
