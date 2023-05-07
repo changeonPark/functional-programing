4d3b6c942b88ce9ad2a20a1de397920b48e28692
changeonPark <qkrcksrjs@gmail.com>
2023-05-07 23:10:01 +0900

    chore: 폴더 이름 변경

--- src/basic/compose/index.ts
+++ src/basic/compose/index.ts
@@ -1,3 +1,15 @@
+/**
+ * 임의의 함수: f
+ * 더 많은 함수가 필요: 알파벳 순서대로 ex)g, h, i
+ * 변수명: x, y, z
+ * 관례임.
+ */
+const compose =
+  <X, Y, R>(g: (y: Y) => R, f: (s: X) => Y) =>
+  (x: X): R => {
+    return g(f(x))
+  }
+
 function getPrice(name: string): number | undefined {
   if (name === 'tomato') {
     return 7000
@@ -15,21 +27,10 @@ const isExpensive = (price: number | undefined): boolean => {
   return price > 10000
 }
 
-function isExpensivePrice(name: string): boolean {
-  return isExpensive(getPrice(name))
-}
-
-/**
- * 임의의 함수: f
- * 더 많은 함수가 필요: 알파벳 순서대로 ex)g, h, i
- * 변수명: x, y, z
- * 관례임.
- */
-const compose =
-  <X, Y, R>(g: (y: Y) => R, f: (s: X) => Y) =>
-  (x: X): R => {
-    return g(f(x))
-  }
+// function isExpensivePrice(name: string): boolean {
+//   return isExpensive(getPrice(name))
+// }
+// const isExpensivePrice = compose(isExpensive, getPrice)
 
 const test = compose<string, number | undefined, boolean>(isExpensive, getPrice)('tomato')
 
