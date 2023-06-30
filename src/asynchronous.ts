e7a71c55b427e882ed3402213d36d1293730a7a3
changeonPark <qkrcksrjs@gmail.com>
2023-06-30 09:55:04 +0900

    feat: callback hell test

--- src/asynchronous.ts
+++ src/asynchronous.ts
@@ -6,13 +6,43 @@ const cpsId = <A>(a: A, ret: (a: A) => void) => {
   ret(a)
 }
 
-export const main = () => {
-  // direct style
-  const a = id('test')
-  console.log(a)
+const a = (str: string, ret: (x: number) => void) => {
+  setTimeout(() => {
+    console.log('call a : ', str)
+    ret(str.length * 2)
+  }, 500)
+}
 
-  // continuation-passing style
-  const b = cpsId('test', (x) => {
-    console.log(x + ' cps')
+const b = (str: string, ret: (x: number) => void) => {
+  setTimeout(() => {
+    console.log('call b : ', str)
+    ret(str.length * 3)
+  }, 500)
+}
+
+const c = (str: string, ret: (x: number) => void) => {
+  setTimeout(() => {
+    console.log('call c : ', str)
+    ret(str.length * 20)
+  }, 500)
+}
+
+export const main = () => {
+  // // direct style
+  // const a = id('test')
+  // console.log(a)
+
+  // // continuation-passing style
+  // const b = cpsId('test', (x) => {
+  //   console.log(x + ' cps')
+  // })
+
+  // callback hell
+  a('hello', (x) => {
+    b('hello2', (y) => {
+      c('hello3', (z) => {
+        console.log(z)
+      })
+    })
   })
 }
