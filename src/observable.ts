15666ac142b90c14a1b071b5cd843481d37e5191
changeonPark <qkrcksrjs@gmail.com>
2023-07-08 23:58:02 +0900

    feat: Observable Basic
    * Set Observable Types
    * Iterator Basic
    * Observable Basic

--- src/observable.ts
+++ src/observable.ts
@@ -54,9 +54,87 @@ const program = (s: boolean) => {
   console.log('program' + s)
 }
 
-export const main = () => {
-  run(map(asyncF('test'), (x) => console.log(x)))
-  promiseF('test').then((x) => console.log(x))
+// export const main = () => {
+//   run(map(asyncF('test'), (x) => console.log(x)))
+//   promiseF('test').then((x) => console.log(x))
 
-  console.log('End!')
+//   console.log('End!')
+// }
+
+// advanced
+
+const integers = (n: number): Array<number> => {
+  const ret: Array<number> = []
+
+  let i = 0
+
+  while (i < n) {
+    i += 1
+    ret.push(i)
+  }
+
+  return ret
+}
+
+type Iterator<A> = () => A
+type Iterable<A> = () => Iterator<A>
+
+const integerGenerator = () => {
+  let i = 0
+
+  return () => {
+    i += 1
+    return i
+  }
+}
+
+// 비동기 코드여도 내부에는 동기적 코드가 섞여있는 경우가 많음
+// 동기적인 코드는 별도의 함수로 분리해 비동기 내부에서 합성해 단순한 흐름의 코드 작성 가능
+const promiseIntegers = (n: number): Promise<Array<number>> =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(integers(n))
+    }, 1000)
+  })
+
+// Obersver Pattern 사용된 함수
+// Observer 패턴은 객체의 상태가 변경될 때마다 모든 의존성이 알림을 받고 자동으로 업데이트되는 방식
+// Subject(timerId) = 주기적으로 새로운 값을 생성하고, 이를 ret에 전달하는 역할
+// Observer(ret) = 전달된 값을 처리하는 역할
+// "Emit 한다"는 주로 이벤트 드리븐 프로그래밍 또는 반응형 프로그래밍에서 사용되며, 특정 객체나 구조가 이벤트 또는 값을 "발산" 또는 "발출"한다는 의미
+// 또한, 그 값을 외부로 보내서 다른 코드에서 그 값을 이용할 수 있도록 하는 것을 의미
+const integerObservable: Async<number> = (ret) => {
+  const iter = integerGenerator()
+
+  const timerId = setInterval(() => {
+    const result = iter()
+    // result 값(증가하는 숫자)을 emit한다
+    ret(result)
+
+    if (result >= 5) {
+      console.log('end!')
+      clearInterval(timerId)
+    }
+  }, 1000)
+}
+
+const onManyIntegers = (n: number) => {
+  const arr = integers(n)
+
+  console.log(arr)
+}
+
+const iter = integerGenerator()
+
+const onStep = () => {
+  const n = iter()
+  console.log(n)
+}
+
+export const main = () => {
+  // onStep()
+  // onStep()
+  // promiseIntegers(5).then((x) => console.log(x))
+
+  integerObservable((n) => console.log(n))
 }
