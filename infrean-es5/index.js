34a52e856674146016f7363a56a45ad51420fa77
changeonPark <qkrcksrjs@gmail.com>
2023-07-24 07:29:44 +0900

    feat: First Class Function & Callback Function

--- infrean-es5/index.js
+++ infrean-es5/index.js
@@ -22,9 +22,9 @@ function add4(obj, b) {
   // 외부 obj1 변수를 인자로 받아 값으 변경시켜버림 => 부수 효과
   obj.val += b
 }
-console.log(obj1.val)
+console.log('before add4 ::', obj1.val)
 add4(obj1, 20)
-console.log(obj1.val)
+console.log('after add4 ::', obj1.val)
 
 // 다시 순수 함수
 let obj2 = { val: 10 }
@@ -33,7 +33,45 @@ function add5(obj, b) {
   // 새로운 객체를 return
   return { val: obj.val + b }
 }
-console.log(obj2.val)
+
+console.log('before add5 ::', obj2.val)
 const result = add5(obj2, 20)
-console.log('result : ', result)
-console.log(obj2.val)
+console.log('before add5 ::', obj2.val)
+
+// 일급 함수 => 함수를 값으로 다룰 수 있음
+const f1 = function (a) {
+  return a * a
+}
+
+// 함수가 함수를 인자로 받을 수 있다
+function f2(cb) {
+  return cb()
+}
+
+f2(function () {
+  return 10
+})
+
+/* add_maker */
+function add_maker(a) {
+  // closure
+  return function (b) {
+    return a + b
+  }
+}
+
+const add10 = add_maker(10)
+console.log('add_maker 10, 20 ::', add10(20))
+
+function f4(f1, f2, f3) {
+  return f3(f1() + f2())
+}
+
+console.log(
+  'f4 :: ',
+  f4(
+    () => 2,
+    () => 1,
+    (a) => a * a
+  )
+)
